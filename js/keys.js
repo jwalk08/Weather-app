f8b3f1d9279b5b7b5903048fe62731af90bf4838
jwalk08 <52938802+jwalk08@users.noreply.github.com>
2023-11-30 20:43:23 -0600

    Delete js/keys.js

--- js/keys.js
+++
@@ -1,5 +0,0 @@
-export const keys = {
-    github: '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',
-    mapbox: 'pk.eyJ1IjoiandhbGs5MCIsImEiOiJjbHBoNjRpa28wNG1oMnFyeGZjeDhvaGdoIn0.-CnKXIvgMqys5HuF2k1ygw',
-    openweather: '8eda2e9236e69d59d94a68c24ee35e19',
-};
